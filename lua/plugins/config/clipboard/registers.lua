f28eb9f677cc312484c9020f2499eb8613739219
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-11-04 11:51:15 +0700

    config(yoink): added configuration

--- lua/plugins/config/clipboard/registers.lua
+++ lua/plugins/config/clipboard/registers.lua
@@ -1,7 +1,7 @@
 
 return function(use)
 	vim.g.yoinkSyncNumberedRegisters = 1
-	-- vim.g.yoinkSavePersistently = 1
+	vim.g.yoinkSavePersistently = 1
 	vim.g.yoinkIncludeDeleteOperations = 1
 	vim.g.registers_delay = 0
 	use({ 'tversteeg/registers.nvim', requires = { 'svermeulen/vim-yoink', } })
