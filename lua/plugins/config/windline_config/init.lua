e6cff2c10c1af766f58fd1bb476b5604609f159e
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-11-10 20:34:16 +0700

    feat(windline): moved from sample to local airline config

--- lua/plugins/config/windline_config/init.lua
+++ lua/plugins/config/windline_config/init.lua
@@ -1,11 +1,11 @@
 return function(use)
 	use({
 		'windwp/windline.nvim',
-		requires = { 'kyazdani42/nvim-web-devicons' },
+		requires = { 'kyazdani42/nvim-web-devicons','nvim-lua/lsp-status.nvim'  },
 		config = function()
 			vim.cmd([[set cmdheight=1]])
 			vim.cmd([[set noshowmode]])
-			require('wlsample.airline')
+			require('plugins.config.windline_config.airline')
 		end,
 	})
 end
