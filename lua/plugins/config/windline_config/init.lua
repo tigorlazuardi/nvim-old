1a6b2e50c60cbcea2580c23a1dc622a622e0d518
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-11-10 21:04:08 +0700

    feat(windline): added lsp status integration to airline

--- lua/plugins/config/windline_config/init.lua
+++ lua/plugins/config/windline_config/init.lua
@@ -3,6 +3,8 @@ return function(use)
 		'windwp/windline.nvim',
 		requires = { 'kyazdani42/nvim-web-devicons','nvim-lua/lsp-status.nvim'  },
 		config = function()
+local lsp_status = require('lsp-status')
+lsp_status.register_progress()
 			vim.cmd([[set cmdheight=1]])
 			vim.cmd([[set noshowmode]])
 			require('plugins.config.windline_config.airline')
