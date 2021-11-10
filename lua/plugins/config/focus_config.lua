8cc7b85d9cc01c2d2ef9a671585338219ec31ea5
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-11-10 20:02:16 +0700

    config(focus.nvim): sign column now won't disappear on not focused windows

--- lua/plugins/config/focus_config.lua
+++ lua/plugins/config/focus_config.lua
@@ -1,3 +1,10 @@
 return function(use)
-use { "beauwilliams/focus.nvim", config = function() require("focus").setup() end }
+	use({
+		'beauwilliams/focus.nvim',
+		config = function()
+			require('focus').setup({
+				signcolumn = false, -- disable sign column only on main window
+			})
+		end,
+	})
 end
