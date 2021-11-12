d3102a8a5582b96463d97672fac7698bf95e866c
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-11-12 08:59:26 +0700

    settings: added backup settings

---
+++ lua/settings/backup.lua
@@ -0,0 +1,12 @@
+local config_dir = vim.fn.stdpath('config')
+local swap = config_dir .. '/backup'
+local set = vim.api.nvim_set_option
+
+
+pcall(vim.fn.mkdir, swap, 'p', 0777)
+
+local dir = swap .. '/'
+
+set('backupdir', dir)
+set('directory', dir)
+set('undodir', dir)
