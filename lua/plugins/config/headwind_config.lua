ed00ee21ad095cb18c39a5599a8c592bdbeefdeb
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-11-15 15:12:48 +0700

    feat(tailwind-headwind): added tailwind and headwind support

---
+++ lua/plugins/config/headwind_config.lua
@@ -0,0 +1,5 @@
+return function(use)
+	use({ 'steelsojka/headwind.nvim', config = function ()
+		require('headwind').setup({})
+	end })
+end
--- lua/plugins/config/lsp/tailwind.lua
+++ lua/plugins/config/lsp/tailwind.lua
@@ -1,11 +1,42 @@
-local capabilities = require "plugins.config.lsp.capabilities"
-local on_attach    = require "plugins.config.lsp.on_attach"
+local capabilities = require('plugins.config.lsp.capabilities')
+local on_attach = require('plugins.config.lsp.on_attach')
 
 if vim.fn.exepath('tailwindcss-language-server') ~= '' then
 	local lspconfig = require('lspconfig')
 
 	lspconfig.tailwindcss.setup({
 		capabilities = capabilities,
-		on_attach = on_attach,
+		on_attach = function(client, bufnr)
+			on_attach(client, bufnr)
+
+			local present, _ = pcall(require, 'headwind')
+			if not present then
+				return
+			end
+
+			local wk = require('which-key')
+			local wk_option = { buffer = bufnr }
+			wk.register({
+				q = {
+					name = 'quick',
+					s = {
+						"<cmd>lua require('headwind').buf_sort_tailwind_classes()<cr>",
+						'Sort Tailwind Classes (Buffer)',
+					},
+				},
+			}, wk_option)
+			wk.register({
+				q = {
+					name = 'quick',
+					s = {
+						"<cmd>lua require('headwind').visual_sort_tailwind_classes()<cr>",
+						'Sort Tailwind Classes (Visual)',
+					},
+				},
+			}, {
+				buffer = bufnr,
+				mode = 'v',
+			})
+		end,
 	})
 end
