c23f66d5a094ee3daf2577c7c7000bd773aefb86
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-11-15 15:41:49 +0700

    config(godot): added global option to disable godot lsp if godot is installed but don't plan to use neovim for coding

--- lua/globals.lua
+++ lua/globals.lua
@@ -2,6 +2,9 @@
 vim.g.is_windows = vim.fn.has('win32') ~= 0 or vim.fn.has('win64') ~= 0
 vim.g.not_vscode = vim.g.vscode ~= 1
 
+-- Set to false if godot is insalled but you don't plan to use neovim
+vim.g.use_godot_lsp = true
+
 ---@return string
 ---@param str string
 function _G.t(str)
--- lua/plugins/config/lsp/godot.lua
+++ lua/plugins/config/lsp/godot.lua
@@ -1,7 +1,15 @@
+if not vim.g.use_godot_lsp then
+	return
+end
+
+if vim.fn.exepath('godot') ~= '' and vim.fn.exepath('nc') == '' then
+	print('godot is installed but netcat is not installed. please install net cat to continue')
+end
+
 -- require godot and netcat executable to run
 if vim.fn.exepath('godot') ~= '' and vim.fn.exepath('nc') ~= '' then
 	require('lspconfig').gdscript.setup({
 		on_attach = require('plugins.config.lsp.on_attach'),
-		capabilities =require('plugins.config.lsp.capabilities')
+		capabilities = require('plugins.config.lsp.capabilities'),
 	})
 end
