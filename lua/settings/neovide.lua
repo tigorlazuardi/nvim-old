7ec2670a13a1be6c26c670ebbfb5e1ee6caf2150
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-11-16 09:42:59 +0700

    config(neovide): disable blur for multigrid mode

--- lua/settings/neovide.lua
+++ lua/settings/neovide.lua
@@ -2,3 +2,6 @@ vim.g.neovide_remember_window_size = true
 vim.g.neovide_cursor_antialiasing = true
 vim.g.neovide_cursor_vfx_mode = 'wireframe'
 vim.g.neovide_transparency = 0
+vim.g.neovide_window_floating_opacity = 0
+vim.g.neovide_floating_blur = 0
+vim.g.neovide_window_floating_blur = 0
