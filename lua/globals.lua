0e9a60de47025d218bf5c7a7e6baccecea23c2d3
TigorHutasuhut <tigor.hutasuhut@gmail.com>
2021-12-13 10:27:50 +0700

    conf(settings): font settings now check for wsl

--- lua/globals.lua
+++ lua/globals.lua
@@ -1,5 +1,5 @@
 ---@return boolean
-vim.g.is_windows = vim.fn.has('win32') ~= 0 or vim.fn.has('win64') ~= 0
+vim.g.is_windows = (vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1) and vim.fn.has('wsl') == 0
 
 -- Set to false if godot is insalled but you don't plan to use neovim
 vim.g.use_godot_lsp = true
