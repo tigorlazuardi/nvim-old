2436aad7029aaa769edabf365d96832cac08d771
tigorhutasuhut <tigor.hutasuhut@gmail.com>
2021-12-13 09:32:09 +0700

    conf(neovide): set neovide transparency to 1

--- lua/settings/neovide.lua
+++ lua/settings/neovide.lua
@@ -1,7 +1,7 @@
 vim.g.neovide_remember_window_size = true
 vim.g.neovide_cursor_antialiasing = true
 vim.g.neovide_cursor_vfx_mode = 'wireframe'
-vim.g.neovide_transparency = 0
+vim.g.neovide_transparency = 1
 vim.g.neovide_window_floating_opacity = 0
 vim.g.neovide_floating_blur = 0
 vim.g.neovide_window_floating_blur = 0
