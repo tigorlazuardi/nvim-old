e175b149cadb69b44e1071777fda6cf121a59cdc
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-12-29 13:53:27 +0700

    feat(theme): random theme now won't repeat last

--- lua/plugins/config/theme.lua
+++ lua/plugins/config/theme.lua
@@ -1,7 +1,13 @@
 return function(use)
 	_G.theme_list = {}
+	_G.loaded_theme = ''
 	_G.load_random_theme = function()
 		local theme_name = _G.theme_list[math.random(#_G.theme_list)]
+		if theme_name == _G.loaded_theme and #_G.theme_list >= 2 then
+			_G.load_random_theme()
+			return
+		end
+		_G.loaded_theme = theme_name
 		vim.cmd('colo ' .. theme_name)
 	end
 	vim.cmd([[au VimEnter * lua _G.load_random_theme()]])
