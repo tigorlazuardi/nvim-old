937eb52cb6a7a811f063afeb7d49f97aa02bb9ff
TigorHutasuhut <tigor.hutasuhut@gmail.com>
2022-01-08 22:33:43 +0700

    feat(pretty-fold): set fold next max to 1

--- lua/plugins/config/pretty_fold.lua
+++ lua/plugins/config/pretty_fold.lua
@@ -3,14 +3,6 @@ return function(use)
 		'anuvyklack/pretty-fold.nvim',
 		config = function()
 			require('pretty-fold').setup({})
-			-- require('pretty-fold.preview').setup_keybinding()
-			--
-			-- vim.cmd([[
-			-- 	augroup pretty_fold_custom
-			-- 		autocmd!
-			-- 		autocmd CursorHold * lua require('pretty-fold.preview').show_preview()
-			-- 	augroup end
-			-- ]])
 		end,
 	})
 end
--- lua/settings/window.lua
+++ lua/settings/window.lua
@@ -6,3 +6,5 @@ c('set signcolumn=yes:1')
 c('set hidden')
 c('set splitright')
 c('set splitbelow')
+
+c('set foldnestmax=1')
