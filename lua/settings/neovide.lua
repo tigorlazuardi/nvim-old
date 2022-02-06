412432f2e87afd9d0488cb3c33b42494d6e9e8f9
tigorhutasuhut <tigor.hutasuhut@gmail.com>
2022-02-06 11:43:28 +0700

    refactor(neovide): refactor neovide configurations

--- lua/settings/neovide.lua
+++ lua/settings/neovide.lua
@@ -4,42 +4,23 @@ vim.g.neovide_cursor_vfx_mode = 'wireframe'
 vim.g.neovide_floating_blur = 1
 vim.g.neovide_window_floating_opacity = 1
 
-vim.g.neovide_font_size_windows = 14
-vim.g.neovide_font_size_linux = 14
-
-local linux_font_name = [[JetBrainsMono\ Nerd\ Font\ Mono]]
-local windows_font_name = [[FiraCode\ NF]]
+local font_name = [[JetBrainsMono\ Nerd\ Font\ Mono]]
+local font_size = 14
 
 ---set font size
 ---@param font_name string
----@param font_size integer
-local function set_font(font_name, font_size)
-	local cmd = string.format('set guifont=%s:h%d', font_name, font_size)
+---@param size integer
+local function set_font_size(size)
+	local cmd = string.format('set guifont=%s:h%d', font_name, size)
 	vim.cmd(cmd)
 end
 
-if vim.g.is_windows or vim.fn.has('wsl') == 1 then
-	set_font(windows_font_name, vim.g.neovide_font_size_windows)
-else
-	set_font(linux_font_name, vim.g.neovide_font_size_linux)
-end
-
----handle font size
----@param increase boolean
-_G.font_size = function(increase)
-	if increase then
-		vim.g.neovide_font_size_windows = vim.g.neovide_font_size_windows + 1
-		vim.g.neovide_font_size_linux = vim.g.neovide_font_size_linux + 1
-	else
-		vim.g.neovide_font_size_windows = vim.g.neovide_font_size_windows - 1
-		vim.g.neovide_font_size_linux = vim.g.neovide_font_size_linux - 1
-	end
-	if vim.g.is_windows or vim.fn.has('wsl') == 1 then
-		set_font(windows_font_name, vim.g.neovide_font_size_windows)
-	else
-		set_font(linux_font_name, vim.g.neovide_font_size_linux)
-	end
-end
-
-vim.api.nvim_set_keymap('n', 'g-', 'v:lua.font_size(v:false)', { expr = true, noremap = true })
-vim.api.nvim_set_keymap('n', 'g=', 'v:lua.font_size(v:true)', { expr = true, noremap = true })
+set_font_size(font_size)
+vim.keymap.set('n', 'g-', function()
+	font_size = font_size - 1
+	set_font_size(font_size)
+end, { silent = true })
+vim.keymap.set('n', 'g=', function()
+	font_size = font_size + 1
+	set_font_size(font_size)
+end, { silent = true })
