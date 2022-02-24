a0623aeff108b3b573a962b587a97e54e7d83cde
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2022-02-24 17:14:50 +0700

    fix(golang): ray-x/go.nvim now always runs and never depends on if gopls is installed first

--- lua/plugins/config/golang_enhancement.lua
+++ lua/plugins/config/golang_enhancement.lua
@@ -1,31 +1,32 @@
 return function(use)
-	if vim.fn.exepath('gopls') ~= '' then
-		use({
-			'ray-x/go.nvim',
-			requires = { 'ray-x/guihua.lua', 'akinsho/nvim-toggleterm.lua' },
-			config = function()
-				local lsp_installer_servers = require('nvim-lsp-installer.servers')
-				local server_available, gopls = lsp_installer_servers.get_server('gopls')
-				if server_available then
-					gopls:on_ready(function()
-						local opts = require('plugins.config.lsp.installer_config.golang')
-						gopls:setup(opts)
-					end)
+	use({
+		'ray-x/go.nvim',
+		requires = { 'ray-x/guihua.lua', 'akinsho/nvim-toggleterm.lua' },
+		config = function()
+			local lsp_installer_servers = require('nvim-lsp-installer.servers')
+			local server_available, gopls = lsp_installer_servers.get_server('gopls')
+			local path = require('nvim-lsp-installer.path')
+			local install_root_dir = path.concat({ vim.fn.stdpath('data'), 'lsp_servers' })
+			if server_available then
+				gopls:on_ready(function()
+					local opts = require('plugins.config.lsp.installer_config.golang')
+					gopls:setup(opts)
+				end)
 
-					if not gopls:is_installed() then
-						vim.notify('installing gopls...')
-						gopls:install()
-					end
+				if not gopls:is_installed() then
+					vim.notify('installing gopls...')
+					gopls:install()
 				end
-				require('go').setup({
-					gofmt = 'gofumpt',
-					test_runner = 'richgo',
-					run_in_floaterm = true,
-				})
-			end,
-			ft = { 'go' },
-		})
-	end
+			end
+			require('go').setup({
+				gopls_cmd = { install_root_dir .. '/go/gopls' },
+				gofmt = 'gofumpt',
+				test_runner = 'richgo',
+				run_in_floaterm = true,
+			})
+		end,
+		ft = { 'go' },
+	})
 
 	use({
 		'rafaelsq/nvim-goc.lua',
