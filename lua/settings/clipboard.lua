dc650d016b83b02a66176070e68684d2e2145f23
TigorHutasuhut <tigor.hutasuhut@gmail.com>
2022-04-23 20:57:45 +0700

    refactor(clipboard): moved to lua completely

--- lua/settings/clipboard.lua
+++ lua/settings/clipboard.lua
@@ -1,9 +1,11 @@
-local c = vim.api.nvim_command
-c('set clipboard+=unnamedplus')
+vim.o.clipboard = vim.o.clipboard .. 'unnamedplus'
 
-vim.cmd([[
-    augroup highlight_yank
-        autocmd!
-        autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank()
-    augroup end
-]])
+local highlight_yank = vim.api.nvim_create_augroup('highlight_yank', { clear = true })
+vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
+	pattern = { '*' },
+	callback = function()
+		require('vim.highlight').on_yank()
+	end,
+	desc = 'Creates highlight on highlighted content when yanking',
+	group = highlight_yank,
+})
