2d13bd4b1fd2333fdb06840e788ad8f4e0021ba2
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2022-06-02 09:37:38 +0700

    fix(filetype): fix filetypes

--- lua/plugins/config/filetype_config.lua
+++ lua/plugins/config/filetype_config.lua
@@ -12,9 +12,12 @@ return function(use)
 						['tsconfig.json'] = 'jsonc',
 						['jsconfig.json'] = 'jsonc',
 						['mods.txt'] = 'javascript', -- rpgmaker mods.txt
+						['.rtorrent.rc'] = 'config',
 					},
 					complex = {
-						['.*vscode/launch.json'] = 'jsonc',
+						['%.vscode/launch%.json'] = 'jsonc',
+						['nginx/.*%.conf'] = 'nginx',
+						['openvpn/.*%.conf'] = 'config',
 					},
 				},
 			})
